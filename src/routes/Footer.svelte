df7c188e1bd89da1ccaae91e939b49ad13ba2aea
Muhsin Abboud <abboudmuhsin@gmail.com>
2025-07-12 15:16:49 +0200

    Update social media links in Footer, hide sections with no content currently, add more padding to be fully visible with displaying audio player at the same time

--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -1,30 +1,30 @@
-<footer class="mt-10 bg-gray-900 py-8 text-gray-300">
+<footer class="bg-gray-900 pt-12 pb-20 text-gray-300">
   <div class="mx-auto max-w-6xl px-4">
-    <div class="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-4">
+    <div class="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-2">
       <!-- Navigation -->
-      <div>
+      <!-- <div>
         <h3 class="mb-3 text-lg font-semibold text-white">Navigation</h3>
         <ul class="space-y-2">
           <li><a href="/duas" class="hover:text-white">Duas</a></li>
           <li><a href="/tags" class="hover:text-white">Tags</a></li>
           <li><a href="/blog" class="hover:text-white">Blog</a></li>
         </ul>
-      </div>
+      </div> -->
 
       <!-- Social Media -->
       <div>
         <h3 class="mb-3 text-lg font-semibold text-white">Social Media</h3>
         <ul class="space-y-2">
           <li>
-            <a href="instagram.com" class="hover:text-white">Instagram</a>
+            <a href="https://www.instagram.com/duas.pro_official/" class="hover:text-white">Instagram</a>
           </li>
-          <li><a href="tiktok.com" class="hover:text-white">TikTok</a></li>
-          <li><a href="youtube.com" class="hover:text-white">YouTube</a></li>
+          <li><a href="https://www.tiktok.com/@duas.pro" class="hover:text-white">TikTok</a></li>
+          <li><a href="https://www.youtube.com/@duaspro" class="hover:text-white">YouTube</a></li>
         </ul>
       </div>
 
       <!-- Über uns / Spende / Kontakt -->
-      <div>
+      <!-- <div>
         <h3 class="mb-3 text-lg font-semibold text-white">Info</h3>
         <ul class="space-y-2">
           <li><a href="/about" class="hover:text-white">Über Uns</a></li>
@@ -33,7 +33,7 @@
             <a href="/kontakt" class="hover:text-white">Kontakt / Contribute</a>
           </li>
         </ul>
-      </div>
+      </div> -->
 
       <!-- Logo + No Copyright -->
       <div class="flex flex-col items-start">
