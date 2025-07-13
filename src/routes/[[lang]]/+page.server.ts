c8f176bbc40931ba0715073e6c8dda04cf1fc6ce
Muhsin Abboud <abboudmuhsin@gmail.com>
2025-07-13 13:09:08 +0200

    Fix redirecting to main page by using current language

--- src/routes/[[lang]]/+page.server.ts
+++ src/routes/[[lang]]/+page.server.ts
@@ -2,5 +2,5 @@ import { redirect } from '@sveltejs/kit';
 
 /** @type {import('./$types').PageServerLoad} */
 export async function load({ params, url }) {
-  redirect(307, '/duas');
+  redirect(307, `${params.lang}/duas`);
 }
