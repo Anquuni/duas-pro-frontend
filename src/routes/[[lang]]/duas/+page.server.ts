8d4d26a1c8912e97ae3958f936cbb0b3a33c55f5
Muhsin Abboud <abboudmuhsin@gmail.com>
2025-07-13 13:10:01 +0200

    Use for systemLang toLowerCase instead of toUpperCase

--- src/routes/[[lang]]/duas/+page.server.ts
+++ src/routes/[[lang]]/duas/+page.server.ts
@@ -3,7 +3,7 @@ import { error } from '@sveltejs/kit';
 
 /** @type {import('./$types').PageServerLoad} */
 export async function load({ params, url }) {
-  const systemLang = params.lang?.toUpperCase();
+  const systemLang = params.lang?.toLowerCase();
   const languages = ["ar", "translit", systemLang && systemLang !== "ar" ? systemLang : "en"];
   const { data: response, error: errorResponse } = await supabase.functions.invoke(`duas?languages=${languages}&page=1&size=100`);
   if (errorResponse) {
