e827816ea87f86268cc1b12329dc06476808660b
Muhsin Abboud <abboudmuhsin@gmail.com>
2025-07-29 07:50:18 +0200

    Add link to open contact formular in footer and open social media links in new tab

--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -1,6 +1,6 @@
 <footer class="bg-gray-900 pt-12 pb-20 text-gray-300">
   <div class="mx-auto max-w-6xl px-4">
-    <div class="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-2">
+    <div class="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3">
       <!-- Navigation -->
       <!-- <div>
         <h3 class="mb-3 text-lg font-semibold text-white">Navigation</h3>
@@ -16,24 +16,24 @@
         <h3 class="mb-3 text-lg font-semibold text-white">Social Media</h3>
         <ul class="space-y-2">
           <li>
-            <a href="https://www.instagram.com/duas.pro_official/" class="hover:text-white">Instagram</a>
+            <a href="https://www.instagram.com/duas.pro_official/" target="_blank" rel="noopener noreferrer" class="hover:text-white">Instagram</a>
           </li>
-          <li><a href="https://www.tiktok.com/@duas.pro" class="hover:text-white">TikTok</a></li>
-          <li><a href="https://www.youtube.com/@duaspro" class="hover:text-white">YouTube</a></li>
+          <li><a href="https://www.tiktok.com/@duas.pro" target="_blank" rel="noopener noreferrer" class="hover:text-white">TikTok</a></li>
+          <li><a href="https://www.youtube.com/@duaspro" target="_blank" rel="noopener noreferrer" class="hover:text-white">YouTube</a></li>
+          <li><a href="https://x.com/duas_pro" target="_blank" rel="noopener noreferrer" class="hover:text-white">Twitter / X</a></li>
         </ul>
       </div>
 
-      <!-- Über uns / Spende / Kontakt -->
-      <!-- <div>
+      <!-- Kontakt -->
+      <div>
         <h3 class="mb-3 text-lg font-semibold text-white">Info</h3>
         <ul class="space-y-2">
-          <li><a href="/about" class="hover:text-white">Über Uns</a></li>
-          <li><a href="/spende" class="hover:text-white">Spende</a></li>
+          <!-- <li><a href="/about" class="hover:text-white">Über Uns</a></li> -->
           <li>
-            <a href="/kontakt" class="hover:text-white">Kontakt / Contribute</a>
+            <a href="https://forms.gle/PXRZ8y2fUYiymvnr9" target="_blank" rel="noopener noreferrer" class="hover:text-white">Contact</a>
           </li>
         </ul>
-      </div> -->
+      </div>
 
       <!-- Logo + No Copyright -->
       <div class="flex flex-col items-start">
