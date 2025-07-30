eb3d0cbdeffacda28e266d12c4fd8ae7d306c361
Muhsin Abboud <abboudmuhsin@gmail.com>
2025-07-30 07:07:35 +0200

    Introduce pagination

--- src/routes/[[lang]]/duas/+page.server.ts
+++ src/routes/[[lang]]/duas/+page.server.ts
@@ -3,9 +3,11 @@ import { error } from '@sveltejs/kit';
 
 /** @type {import('./$types').PageServerLoad} */
 export async function load({ params, url }) {
+  const page = parseInt(url.searchParams.get('page') || '1');
+  const size = parseInt(url.searchParams.get('size') || '20');
   const systemLang = params.lang?.toLowerCase();
   const languages = ["ar", "translit", systemLang && systemLang !== "ar" ? systemLang : "en"];
-  const { data: response, error: errorResponse } = await supabase.functions.invoke(`duas?languages=${languages}&page=1&size=100`);
+  const { data: response, error: errorResponse } = await supabase.functions.invoke(`duas?languages=${languages}&page=${page}&size=${size}`);
   if (errorResponse) {
     console.error(`Error while fetching for languages ${languages} all duas: ${errorResponse}`)
     error(404);
@@ -13,5 +15,7 @@ export async function load({ params, url }) {
 
   return {
     duas: response.data,
+    page: page,
+    size: size,
   };
 }
--- src/routes/[[lang]]/duas/+page.svelte
+++ src/routes/[[lang]]/duas/+page.svelte
@@ -1,11 +1,22 @@
 <script lang="ts">
   import DuaCard from "$lib/dua-discovery/DuaCard.svelte";
+  import DuaDiscoveryPagination from "$lib/dua-discovery/DuaDiscoveryPagination.svelte";
+  import FilterPanel from "$lib/dua-discovery/FilterPanel.svelte";
   import SeoHead from "$lib/SEOHead.svelte";
+  import { settingsStore } from "$lib/settings/settings.store.js";
   import { t } from "$lib/translations/i18n";
+  import * as Sheet from "$lib/components/ui/sheet/index.js";
 
-  // TODO: Add filtering by book (Mafatih-ul-Jinan, Sahifa-Sadschadiyya, ...)
-  //  and by type (Dua, Ziyarat, Muanajat, ...) and a search
-  // TODO: Create pagination component
+  // TODO: Add Sheet or Drawer component to filter duas
+  //   by book (Mafatih-ul-Jinan, Sahifa-Sadschadiyya, ...),
+  //   by type (Dua, Ziyarat, Muanajat, ...),
+  //   by collection (Ramadan, Imam Mahdi)
+  //   by recommended time (today, weekly, monthly, yearly)
+  //   by narrator
+  //   if has audio or not
+  //   by recitator
+  //   by reading time
+  // TODO: Add a full-text search
 
   let { data } = $props();
 </script>
@@ -13,10 +24,18 @@
 <SeoHead title={$t("seo.home.title")} description={$t("seo.home.description")} type="organization" />
 
 <div class="p-4 sm:p-6 md:p-8">
-  <!-- Duas Grid -->
-  <div class="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
-    {#each data.duas as dua}
-      <DuaCard {dua} />
-    {/each}
+  <!-- 📜 Duas Grid -->
+  <div class="flex-1">
+    <div class="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3">
+      {#each data.duas as dua}
+        <DuaCard {dua} />
+      {/each}
+    </div>
+
+    <DuaDiscoveryPagination
+      currentPage={data.page}
+      totalItems={81}
+      itemsPerPage={data.size}
+      baseUrl="/{$settingsStore.systemLanguage}/duas" />
   </div>
 </div>
