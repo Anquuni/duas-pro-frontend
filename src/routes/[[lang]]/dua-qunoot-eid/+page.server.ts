5667b602f4b4c95d3063a2029341a1832013484b
Muhsin Abboud <abboudmuhsin@gmail.com>
2026-04-15 20:52:07 +0200

    Removed deprecated pages

--- src/routes/[[lang]]/dua-qunoot-eid/+page.server.ts
+++
@@ -1,6 +0,0 @@
-import { error } from "@sveltejs/kit";
-
-/** @type {import('./$types').PageServerLoad} */
-export function load() {
-  throw error(410);
-}
--- src/routes/[[lang]]/kontakt/+page.server.ts
+++
@@ -1,6 +0,0 @@
-import { error } from "@sveltejs/kit";
-
-/** @type {import('./$types').PageServerLoad} */
-export function load() {
-  error(410);
-}
